91d1840071d349b6c7bb3b005d4cf489ba63f0fe
baryska <barbora.skalova@newtontech.cz>
2025-07-02 23:21:32 +0200

    fix rendering 3

--- src/app/mapa/page.tsx
+++ src/app/mapa/page.tsx
@@ -1,12 +1,12 @@
 "use client";
-import styles from './style.module.css';
+// import styles from './style.module.css';
 import MapComponent from "@/components/MapComponent";
 
 export default function Page() {
   return (
     <main>
       <MapComponent />
-      <h1 className={styles.title}>Letní berounská šifrovačka</h1>
+      <h1>Letní berounská šifrovačka</h1>
     </main>
   );
 }
