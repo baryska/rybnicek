55b041bbbe8c20dd38bf768bca17fdb09d10be68
baryska <barbora.skalova@newtontech.cz>
2025-07-21 17:21:59 +0200

    add private page

---
+++ src/app/sifra_private/page.tsx
@@ -0,0 +1,27 @@
+import MapClientPage from "../../components/MapClientPage";
+import Image from 'next/image';
+import styles from './style.module.css';
+
+export default function Page() {
+  const isPlaceholder = false;
+  if (isPlaceholder) {
+    return (
+      <main className={styles.main_placeholder}>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <div className={styles.placeholderText}>Berounská letní šifrovačka</div>
+          <div className={styles.placeholderSmallText}>Už se to chystá, vraťte se prosím 1. srpna!</div>
+          <Image src="/logo_orange.svg" alt="Logo" width={200}
+            height={200} className={styles.logo} />
+        </div>
+      </main>
+    )
+  }
+  else {
+    return (
+      <main className={styles.main}>
+        <MapClientPage />
+      </main>
+    );
+  }
+
+}
