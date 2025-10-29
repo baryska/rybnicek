5d09e5214a9fef9ab177e7ecb806f511361d210f
baryska <barbora.skalova@newtontech.cz>
2025-10-29 15:42:43 +0100

    add advent page

--- src/app/mapa/page.tsx
+++ src/app/mapa/page.tsx
@@ -1,12 +1,12 @@
 import MapClientPage from '@/components/MapClientPage';
-import React from 'react'
+import React from 'react';
 
 const Mapa = () => {
   return (
-      <main>
-        <MapClientPage showCrossword={false}/>
-      </main>
-    );
+    <main>
+      <MapClientPage showCrossword={false} />
+    </main>
+  );
 }
 
 export default Mapa;
